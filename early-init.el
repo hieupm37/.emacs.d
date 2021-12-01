67604b015d4ac57c8fde650e59e2014b7293096d
Hieu Phan <hieupm@coccoc.com>
2021-12-01 14:46:15 +0700

    Fix invoking gcc driver error os OS X

--- early-init.el
+++ early-init.el
@@ -26,6 +26,11 @@
 ;; Start initial frame maximized
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
+;; This fixes invoking gcc drive error on OS X
+(if (eq system-type 'darwin)
+    (setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/11:/usr/local/opt/libgccjit/lib/gcc/11:/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11")
+  )
+
 (setq native-comp-async-report-warnings-errors 'silent)  ; emacs 28 with native compilation
 
 ;;; early-init.el ends here
